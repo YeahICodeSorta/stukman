73766f39e7273310eab17845828148c639ba905b
Andrew <agarafalo89@gmail.com>
2015-11-03 00:02:30 -0500

    Finishing UX/UI stuff, making it prettier

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+  	if current_user
+  		redirect_to tasks_path
+  	end
   end
 
   def about
@@ -8,5 +11,6 @@ class PagesController < ApplicationController
 
 
   def test
+
   end
 end
